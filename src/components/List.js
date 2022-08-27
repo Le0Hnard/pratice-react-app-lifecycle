d3ca441deaa033937150526109a6835627239a6a
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-08-27 14:28:14 +0800

    Added lifecycle methods

---
+++ src/components/List.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ActionButton from './ActionButton';
+
+export default class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      names: ["Bob", "Alice", "Dora"]
+    }
+  }
+
+  reverseList = () => {
+    this.setState({ names: this.state.names.reverse() });
+  }
+
+  render() {
+    return (
+      <div>
+        <ActionButton callback={ this.reverseList } text="Reverse Names" />
+        { this.state.names.map((name, index) => {
+          return <h5 id={ name.toLowerCase() } key={ name }>{ name }</h5>
+        }) }
+      </div>
+    );
+  }
+}
